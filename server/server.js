5f3978804c97862f57386a18a18eb05a318fb830
Aoi Tanaka <aoitanaka.0523@gmail.com>
2025-06-03 22:24:55 +0200

    Fix cors error

--- server/server.js
+++ server/server.js
@@ -11,7 +11,15 @@ require("dotenv").config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://image-search-three-iota.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
